11fcec25c2cfe15fe2dd44fd7212b4f061d0da4b
gandres42 <gandres2416@gmail.com>
2025-03-03 14:30:14 -0800

    add perception file

--- src/main.py
+++ src/main.py
@@ -3,17 +3,19 @@ from InverseKinematics import IK
 import math
 import random
 
-ik = IK()
-angles = ik.getRotationAngle((0, 18, 7), 0)
-for i, item in enumerate(angles.values()):
+# ik = IK()
+# angles = ik.getRotationAngle((0, 18, 7), 0)
+# for i, item in enumerate(angles.values()):
     
-    item = item - 90
-    if i + 3 in [3, 5, 6]:
-        item = -item
-    print(i + 3, item)
+#     item = item - 90
+#     if i + 3 in [3, 5, 6]:
+#         item = -item
+#     print(i + 3, item)
 
 
 # invert 3, 5, 6
-Board.setMotorDegree(3, 19)
-Board.setMotorDegree(4, 81)
-Board.setMotorDegree(5, -10)
+Board.setMotorDegree(2, 0, blocking=False)
+Board.setMotorDegree(3, 90, blocking=False)
+Board.setMotorDegree(4, 90, blocking=False)
+Board.setMotorDegree(5, -45, blocking=False)
+Board.setMotorDegree(6, 0, blocking=True)
