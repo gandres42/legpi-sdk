bfc5f6af9e95e8d4b6efebad95f07de6b842472a
gandres42 <gandres2416@gmail.com>
2025-03-13 12:46:34 -0700

    good enough movements for me, add remote controls

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import time
 import sys
 from enum import Enum
+import pybox
 
 sys.path.append('/home/gavin/legpi/armpy')
 
 from armik.ArmMoveIK import *
 from control.Board import *
 
+controller = pybox.UltimateC()
 # order: 6, 5, 4, 3
+
+def turn(angle):
+    times = []
+    times.append(setServoAngle(6, angle, velocity=100))
+    times.append(setServoAngle(5, 70, velocity=50))
+    times.append(setServoAngle(4, 45, velocity=100))
+    times.append(setServoAngle(3, 45, velocity=100))
+    time.sleep(max(0, max(times)))
+
+    times = []
+    times.append(setServoAngle(6, 0, velocity=100))
+    time.sleep(max(0, max(times)))
+
+    times = []
+    times.append(setServoAngle(5, 20, velocity=100))
+    # times.append(setServoAngle(6, 0, velocity=100))
+    time.sleep(max(0, max(times)))
+
 def row():
     times = []
     times.append(setServoAngle(6, 0, velocity=100))
@@ -17,24 +37,20 @@ def row():
     time.sleep(max(0, max(times)))
 
     times = []
-    times.append(setServoAngle(6, 0))
     times.append(setServoAngle(5, 40, velocity=30))
     times.append(setServoAngle(4, 80))
-    times.append(setServoAngle(3, 80))
+    times.append(setServoAngle(3, 90))
     time.sleep(max(0, max(times) - .1))
 
     times = []
-    times.append(setServoAngle(5, 70, velocity=100))
-    # times.append(setServoAngle(5, 10, velocity=100))
+    times.append(setServoAngle(5, 10, velocity=100))
     time.sleep(.3)
 
 def backward_row():
     times = []
-    times.append(setServoAngle(5, 00, velocity=150))
+    times.append(setServoAngle(5, 10, velocity=150))
     times.append(setServoAngle(4, 90, velocity=100))
     times.append(setServoAngle(3, 90, velocity=100))
-    # times.append(setServoAngle(5, 10, velocity=100))
-    # times.append(setServoAngle(5, 10, velocity=100))
     time.sleep(1)
 
     times = []
@@ -46,12 +62,27 @@ def backward_row():
 
     times = []
     times.append(setServoAngle(6, 0, velocity=100))
-    times.append(setServoAngle(5, 70, velocity=50))
+    times.append(setServoAngle(5, 60, velocity=50))
     times.append(setServoAngle(4, 45, velocity=100))
     times.append(setServoAngle(3, 45, velocity=100))
+    time.sleep(max(0, max(times)))
+
+    times = []
+    times.append(setServoAngle(5, 30, velocity=150))
     time.sleep(max(0, max(times)))
 
     
 
 while True:
-    backward_row()
+    y = controller.get_d_pad_y()
+
+    if y == 1:
+        row()
+    elif y == -1:
+        backward_row()
+    
+    x = controller.get_d_pad_x()
+    if x == -1:
+        turn(-45)
+    elif x == 1:
+        turn(45)
