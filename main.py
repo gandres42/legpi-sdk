462926c65949e47319a8b470945afa25f0f48d3b
gandres42 <gandres2416@gmail.com>
2025-03-13 12:21:33 -0700

    backwards motion

--- main.py
+++ main.py
@@ -18,14 +18,40 @@ def row():
 
     times = []
     times.append(setServoAngle(6, 0))
-    times.append(setServoAngle(5, 50, velocity=30))
+    times.append(setServoAngle(5, 40, velocity=30))
     times.append(setServoAngle(4, 80))
     times.append(setServoAngle(3, 80))
     time.sleep(max(0, max(times) - .1))
 
     times = []
-    times.append(setServoAngle(5, 20, velocity=100))
+    times.append(setServoAngle(5, 70, velocity=100))
+    # times.append(setServoAngle(5, 10, velocity=100))
     time.sleep(.3)
 
+def backward_row():
+    times = []
+    times.append(setServoAngle(5, 00, velocity=150))
+    times.append(setServoAngle(4, 90, velocity=100))
+    times.append(setServoAngle(3, 90, velocity=100))
+    # times.append(setServoAngle(5, 10, velocity=100))
+    # times.append(setServoAngle(5, 10, velocity=100))
+    time.sleep(1)
+
+    times = []
+    times.append(setServoAngle(6, 0))
+    times.append(setServoAngle(5, 40, velocity=30))
+    times.append(setServoAngle(4, 80))
+    times.append(setServoAngle(3, 80))
+    time.sleep(max(0, max(times) - .1))
+
+    times = []
+    times.append(setServoAngle(6, 0, velocity=100))
+    times.append(setServoAngle(5, 70, velocity=50))
+    times.append(setServoAngle(4, 45, velocity=100))
+    times.append(setServoAngle(3, 45, velocity=100))
+    time.sleep(max(0, max(times)))
+
+    
+
 while True:
-    row()
+    backward_row()
